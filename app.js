be64a643179283af8612eb37e3f2e4406ffd1600
David <davidkhun2004@gmail.com>
2026-01-27 13:52:28 +0000

    Change the routing file

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-const express = require('express');
-const app = express();
+const app = require('./router/routeController');
+
 const PORT = process.env.PORT || 3000;
-
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Home Page' });
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
