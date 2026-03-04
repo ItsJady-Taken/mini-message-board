5c223f622bc0b86388765931c4cfe0efc82812e0
David <davidkhun2004@gmail.com>
2026-03-04 08:19:23 +0000

    Working on displaying message in index.ejs

--- router/routeController.js
+++ router/routeController.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const messages = require('../data/message');
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Home Page' });
+    res.render('index', { title: 'Home Page', messages: messages });
 });
 
 app.get('/new', (req, res) => {
